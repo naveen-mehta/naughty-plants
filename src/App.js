cec90ede85ccd114a848af2d6b00bcc3f87d6e58
Naveen Mehta <navi.mehta@gmail.com>
2020-08-21 15:35:37 +1000

    Added a logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import {Route} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import Shop from './pages/shop/shop.component.jsx'
-
+import Header from './components/header.component'
 import HomePage from './pages/homepage/homepage.component.jsx'; 
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop' component={Shop}/>   
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/shop' component={Shop}/>   
+      </Switch>
     </div>
   );
 }
